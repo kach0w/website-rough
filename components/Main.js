1c8bd340fd8943ff7129eed1b727f3ce185632f5
Kachow <karsab343@gmail.com>
2022-11-19 16:49:59 -0800

    picture fix

--- components/Main.js
+++ components/Main.js
@@ -9,7 +9,7 @@ const Main = () => {
         <h1 className='font-serif font-bold text-[7em] text-[#15171a] leading-none'>kach0w</h1>
       </div>
       <div className='flex my-[vh] justify-center text-center'>
-        <Image className='w-[30vw]' src={rusteze} alt="img"/>
+        <Image className='w-[30vw]' src="../assets/rusteze.png" alt="img"/>
       </div>
       <div className='flex text-center justify-center'>
         <div className='text-[1.8rem] font-bold text-[#000000]'>
@@ -19,12 +19,12 @@ const Main = () => {
       <div className='flex my-[5vh] justify-center text-center'>
         <a href="https://github.com/kach0w">
           <div className='bg-[#3b82f6] hover:bg-[#0a5adb] w-[9vw] py-5 mr-2 text-[1.5rem] text-[#ffffff] rounded-[8px]'>
-            <div className='font-serif font-bold'><i class="fab fa-github"></i>GitHub</div>
+            <div className='font-serif font-bold'><i class="fab fa-github"></i> GitHub</div>
           </div>
         </a>
         <a href="mailto:kachow@duck.com">
           <div className='bg-[#3b82f6] hover:bg-[#0a5adb] w-[9vw] py-5 ml-2 text-[1.5rem] text-[#ffffff] rounded-[8px]'>
-            <div className='font-serif font-bold'><i class="fa-solid fa-envelope"></i>Email</div>
+            <div className='font-serif font-bold'><i class="fa-solid fa-envelope"></i> Email</div>
           </div>
         </a>
       </div>
